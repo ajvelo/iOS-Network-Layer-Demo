a84ac2b3affa718f74b47c0d06a881310ecce367
Andreas Velounias <andreas.velounias@rokigroup.co.uk>
2019-01-24 16:55:59 +0000

    Add new constants file

---
+++ NetworkLayer/Model/RealmModels/ConstantsNew.swift
@@ -0,0 +1,14 @@
+//
+//  ConstantsNew.swift
+//  NetworkLayer
+//
+//  Created by Andreas Velounias on 24/01/2019.
+//  Copyright © 2019 Malcolm Kumwenda. All rights reserved.
+//
+
+import Foundation
+
+struct ConstantsNew {
+    
+    static let apiKey = "64a84fe3a3455d7ee6208d0cc6ec0ee8"
+}
